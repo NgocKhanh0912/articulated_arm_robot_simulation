f1145616ed0c63c8f75a3ee46bc0924b5ea87e17
khanhnguyenngoc-itr-intern <ngockhanh20003@gmail.com>
2024-10-19 19:44:22 +0700

    style: Config style

--- rotate.m
+++ rotate.m
@@ -1,11 +1,11 @@
 function R = Rotate(theta)
 
-     c = cos(theta); 
-     s = sin(theta); 
-    
-     R = [c, -s, 0 ,0;
-          s,  c, 0, 0;
-          0,  0, 1, 0;
-          0,  0, 0, 1];
+  c = cos(theta); 
+  s = sin(theta); 
+
+  R = [c, -s, 0 ,0;
+      s,  c, 0, 0;
+      0,  0, 1, 0;
+      0,  0, 0, 1];
 end
 
