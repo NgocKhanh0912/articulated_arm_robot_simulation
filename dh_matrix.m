c290391c095c45746106a0d95fc9002748962dd6
khanhnguyenngoc-itr-intern <ngockhanh20003@gmail.com>
2024-10-19 19:34:16 +0700

    style: Config style

--- dh_matrix.m
+++ dh_matrix.m
@@ -6,9 +6,9 @@ function [A] = DH_Matrix(d, theta, a, alpha)
     ca = cos(alpha);
     sa = sin(alpha);
 
-    A = [c   -ca*s   sa*s   a*c; 
-         s    c*ca  -sa*c   a*s;
-         0    sa     ca     d ; 
-         0    0      0      1];
+    A = [c,   -ca*s,   sa*s,   a*c; 
+         s,    c*ca,  -sa*c,   a*s;
+         0,    sa,     ca,     d ; 
+         0,    0,      0,      1];
 
 end
